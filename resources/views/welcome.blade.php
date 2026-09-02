a2df1610e330eb85b4d614246207280a4c361eee
yeyintnaung135 <koluchaw.yk@gmail.com>
2026-09-02 16:43:44 +0630

    tt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2260,7 +2260,7 @@
                     v{{ app()->version() }}
                     <a href="https://github.com/laravel/framework/blob/13.x/CHANGELOG.md" target="_blank"
                         class="inline-flex items-center space-x-1 font-medium underline underline-offset-4 text-[#f53003] dark:text-[#FF4433] ml-1">
-                        <span>View changelog</span>
+                        <span>View chaggggelog</span>
                         <svg width="10" height="11" viewBox="0 0 10 11" fill="none"
                             xmlns="http://www.w3.org/2000/svg" class="w-2.5 h-2.5">
                             <path d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001" stroke="currentColor"
